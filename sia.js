bc380b236808024a4f67d5794a6f05c87481864d
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-10-24 22:04:19 +0200

    Bugfixing

--- sia.js
+++ sia.js
@@ -517,7 +517,7 @@ function parseSIA2(data) {
     sia.crc = data[1] * 256 + data[2];
 
 
-    tmp = d.write(data.substr(3, 7));
+    tmp = data.substr(3, 7);
     // let tmp = (data.subarray(3, 7)).toString();
     sia.len = parseInt(tmp, 16); // length of data
     adapter.log.debug("data : " + data);
